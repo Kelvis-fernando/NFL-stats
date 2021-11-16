45b3195431699432772a0ed24b762deb5cbe0f75
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-11-15 23:13:52 -0300

    Adjusted the routes and the end point of API that made request and get datas from rest api

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="app grid grid-template-columns">
-    <Sidebar class="item" />
-    <router-view class="item m-3" />
-  </div>
+    <div class="app grid grid-template-columns">
+        <Sidebar class="item" />
+        <router-view class="item m-3" />
+    </div>
 </template>
 
 <script>
 import Sidebar from "./layout/Sidebar.vue";
 
 export default {
-  components: { Sidebar },
+    components: { Sidebar },
 };
 </script>
 
 <style lang="scss">
 /* Grid Container */
 .grid {
-  display: grid;
+    display: grid;
 }
 
 .grid-template-columns {
-  grid-template-columns: 350px 600px;
+    grid-template-columns: 350px 600px;
 }
 </style>
