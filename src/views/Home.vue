78f44d0207513fe6a78e3f15c29772f9e219b19c
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-11-16 00:25:13 -0300

    Consuming the rest api on front-end

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="home">
-    <h1>estou aqui</h1>
-  </div>
+    <div class="home">
+        <h1>estou aqui</h1>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+    name: "Home",
 };
 </script>
