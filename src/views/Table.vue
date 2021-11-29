2634387b24e9afa0b3382cd1f1bbb09cdf42299f
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-11-29 15:40:50 -0300

    Started consuming the rest api and take the datas from the json file

---
+++ src/views/Table.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="table">
+    <div class="afc">
+      <div class="north">
+        <h2>AFC NORTH</h2>
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Time</th>
+              <th scope="col">VItorias</th>
+              <th scope="col">Derrotas</th>
+              <th scope="col">Empates</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="south"></div>
+      <div class="west"></div>
+      <div class="east"></div>
+    </div>
+    <div class="nfc">
+      <div class="north"></div>
+      <div class="south"></div>
+      <div class="west"></div>
+      <div class="east"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+function getDataTable() {}
+export default {
+  name: "Table",
+  methods: {
+    getDataTable,
+  },
+  created: function () {
+    axios.get("http://localhost:3001/tabela").then((response) => {
+      console.log(response.data);
+      response.data.forEach((el) => {
+        console.log(el.NFC);
+      });
+    });
+  },
+  data() {
+    return {
+      NFC: [],
+      AFC: [],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.table {
+  width: 800px;
+}
+</style>
