9551569adf84089697adfab186884d089b9288fc
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-12-01 20:10:00 -0300

    Consumed the datas of rest api and show for all users on tables

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -2,7 +2,7 @@
   <div class="table">
     <div class="afc">
       <div class="north">
-        <h2>AFC NORTH</h2>
+        <h2>NFC NORTH</h2>
         <table class="table table-striped">
           <thead>
             <tr>
@@ -13,18 +13,78 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
+            <tr v-for="N in NFC.North" :key="N">
+              <td>{{N.Name}}</td>
+              <td>{{N.Wins}}</td>
+              <td>{{N.Losses}}</td>
+              <td>{{N.Ties}}</td>
             </tr>
           </tbody>
         </table>
       </div>
-      <div class="south"></div>
-      <div class="west"></div>
-      <div class="east"></div>
+      <div class="south">
+        <h2>NFC South</h2>
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Time</th>
+              <th scope="col">VItorias</th>
+              <th scope="col">Derrotas</th>
+              <th scope="col">Empates</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="N in NFC.South" :key="N">
+              <td>{{N.Name}}</td>
+              <td>{{N.Wins}}</td>
+              <td>{{N.Losses}}</td>
+              <td>{{N.Ties}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="west">
+        <h2>NFC West</h2>
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Time</th>
+              <th scope="col">VItorias</th>
+              <th scope="col">Derrotas</th>
+              <th scope="col">Empates</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="N in NFC.West" :key="N">
+              <td>{{N.Name}}</td>
+              <td>{{N.Wins}}</td>
+              <td>{{N.Losses}}</td>
+              <td>{{N.Ties}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="east">
+        <h2>NFC East</h2>
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Time</th>
+              <th scope="col">VItorias</th>
+              <th scope="col">Derrotas</th>
+              <th scope="col">Empates</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="N in NFC.East" :key="N">
+              <td>{{N.Name}}</td>
+              <td>{{N.Wins}}</td>
+              <td>{{N.Losses}}</td>
+              <td>{{N.Ties}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
     <div class="nfc">
       <div class="north"></div>
@@ -48,7 +108,10 @@ export default {
     axios.get("http://localhost:3001/tabela").then((response) => {
       console.log(response.data);
       response.data.forEach((el) => {
-        console.log(el.NFC);
+        this.NFC = el.NFC;
+        this.AFC = el.AFC;
+        console.log(this.NFC);
+        console.log(this.AFC);
       });
     });
   },
